db4c1ed0dc9c829d7e014b9511178773345b0934
Andras Belicza <iczaaa@gmail.com>
2021-11-20 18:40:06 +0100

    Store parameters and state in local storage, restore on page reload

--- static/hagyi.js
+++ static/hagyi.js
@@ -10,6 +10,53 @@ var phases = [
 	{name:"Jobb Tigris", weight: 2/40},
 ];
 
+// initApp initializes the app.
+function initApp() {
+	restoreValues("paramFirstDay", "paramFirstDuration", "paramLastDuration", "paramSound");
+	if (elByID("paramFirstDay").value == "") {
+		elByID("paramFirstDay").value = todayString();
+	}
+	initTable();
+	processParams();
+
+	let storedStopTime = getKey("stopTime");
+	if (storedStopTime != null) {
+		stopTime = new Date(Number(storedStopTime));
+	}
+
+	let storedStartTime = getKey("startTime");
+	if (storedStartTime != null) {
+		startTime = new Date(Number(storedStartTime));
+		updateInfoPanel();
+	}
+
+	let storedCurrentPhaseIdx = getKey("currentPhaseIdx");
+	if (storedCurrentPhaseIdx != null) {
+		currentPhaseIdx = Number(storedCurrentPhaseIdx);
+	}
+
+	let storedAnimating = getKey("animating");
+	if (storedAnimating != null) {
+		animating = Number(storedAnimating);
+		if (animating) {
+			startStop(true);
+		}
+	}
+
+	updateTable();
+}
+
+function resetApp() {
+	if (animateID) {
+		startStop();
+	}
+
+	delKeys("paramFirstDay", "paramFirstDuration", "paramLastDuration", "paramSound",
+		"stopTime", "startTime", "currentPhaseIdx", "animating");
+
+	location.reload();
+}
+
 function initTable() {
 	let table = elByID("infoTable");
 
@@ -38,6 +85,7 @@ function initTable() {
 }
 
 var startTime = null;
+var stopTime = null;
 var duration = 0;
 var currentPhaseIdx = -1;
 
@@ -64,13 +112,14 @@ function updateTable() {
 			cells[2].innerText = formatMin(elapsed);
 			cells[3].innerText = formatMin(phaseDuration - elapsed);
 			percentCell(cells[4], elapsed / phaseDuration);
-			if (animateID && !foundCurrentIdx) {
+			if (animating && !foundCurrentIdx) {
 				foundCurrentIdx = true;
 				if (currentPhaseIdx != i) {
-					defaultBeep();
 					currentPhaseIdx = i;
+					setKeyValue("currentPhaseIdx", currentPhaseIdx);
+					defaultBeep();
 				}
-				}
+			}
 			elapsed = 0;
 		} else {
 			elapsed -= phaseDuration;
@@ -88,6 +137,8 @@ function percentCell(cell, progress) {
 }
 
 function processParams() {
+	storeValues("paramFirstDay", "paramFirstDuration", "paramLastDuration");
+
 	let firstDay = elByID("paramFirstDay").valueAsDate;
 	let day = 1 + Math.floor(daysBetween(firstDay, new Date()));
 	if (day < 1) {
@@ -123,11 +174,21 @@ function processParams() {
 }
 
 var animateID;
+var animating = 0; // 0 or 1
 
-function startStop() {
+function startStop(fromInit) {
 	function stopAnimation() {
 		clearInterval(animateID);
 		animateID = null;
+		animating = 0;
+		setKeyValue("animating", animating);
+
+		if (!(getElapsed() >= duration)) {
+			stopTime = new Date();
+			setKeyValue("stopTime", stopTime.getTime());
+		}
+
+		updateInfoPanel();
 		elByID("startStopButton").innerText = "Indítás!";
 	}
 	
@@ -136,31 +197,62 @@ function startStop() {
 		return;
 	}
 
-	elByID("infoCongratulations").style.display = "none";
 	elByID("startStopButton").innerText = "Leállítás!";
 	
-	currentPhaseIdx = -1;
-	startTime = new Date();
-	elByID("infoStart").innerText = startTime.toLocaleTimeString("en-GB"); // en-GB local uses 24-hour format
-	elByID("infoEnd").innerText = new Date(startTime.getTime() + duration*60000).toLocaleTimeString("en-GB");
-	elByID("infoStartEnd").style.display = "";
+	if (fromInit) {
+		// If we're called from init, no need to set below variables!
+	} else {
+		currentPhaseIdx = -1;
+		setKeyValue("currentPhaseIdx", currentPhaseIdx);
+		startTime = new Date();
+		setKeyValue("startTime", startTime.getTime());
+		stopTime = null;
+		delKey("stopTime");
+		animating = 1;
+		setKeyValue("animating", animating);
+	}
+
+	updateInfoPanel();
 
 	function frame() {
-		let elapsed = getElapsed()
 		updateTable();
-		if (elapsed >= duration) {
+		if (getElapsed() >= duration) {
 			stopAnimation();
-			elByID("infoCongratulations").style.display = "";
 		}
 	}
 	frame(); // Call it now
 	animateID = setInterval(frame, 1000);
 }
 
+function updateInfoPanel() {
+	// use en-GB local for time formatting (it uses 24-hour format)
+	elByID("infoStart").innerText = startTime.toLocaleTimeString("en-GB");
+	elByID("infoEnd").innerText = new Date(startTime.getTime() + duration*60000).toLocaleTimeString("en-GB");
+	if (stopTime != null) {
+		elByID("infoStop").innerText = stopTime.toLocaleTimeString("en-GB");
+		elByID("infoStopPanel").style.display = "";
+	} else {
+		elByID("infoStopPanel").style.display = "none";
+	}
+
+	if (getElapsed() >= duration) {
+		elByID("infoCongratulations").style.display = "";
+	} else {
+		elByID("infoCongratulations").style.display = "none";
+	}
+
+	elByID("infoStartEnd").style.display = "";
+}
+
 function getElapsed() {
 	let elapsed = 0;
 	if (startTime != null) {
-		elapsed = (new Date() - startTime) / 60000;
+		if (stopTime != null) {
+			elapsed = stopTime - startTime;
+		} else {
+			elapsed = new Date() - startTime;
+		}
+		elapsed /= 60000;
 	}
 	if (elapsed > duration) {
 		elapsed = duration;
@@ -239,4 +331,106 @@ function beep(duration, frequency, volume) {
 
 	oscillator.start(audioCtx.currentTime);
 	oscillator.stop(audioCtx.currentTime + ((duration || 500) / 1000));
-};
+}
+
+// toreValues stores the "values" of the elements given by their IDs in the local storage.
+function storeValues(...ids) {
+	for (let id of ids) {
+		storeValue(id);
+	}
+}
+
+// storeValue stores the "value" of the element given by its ID in the local storage.
+// Value is determined like this:
+//   -if the element is a SELECT, the selected option's value is used
+//   -if the element is an INPUT with type "checkbox", its checked attribute is used
+//   -if the element has a value attribute, its value is used
+//   -otherwise the element's textContent is used.
+function storeValue(id) {
+	if (typeof(Storage) == "undefined") {
+		return; // No local storage support
+	}
+
+	var el = elByID(id);
+	var value;
+	if(el.tagName === "SELECT") {
+		value = el.options[el.selectedIndex].value;
+	} else if (el.tagName === "INPUT" && el.type === "checkbox") {
+		value = el.checked.toString();
+	} else if (el.hasAttribute("value")) {
+		value = el.value;
+	} else {
+		alert(el.hasAttribute("value"), el.value);
+		value = el.textContent;
+	}
+	localStorage.setItem(id, value);
+}
+
+// restoreValues restores the values of the elements given by their IDs from the local storage.
+function restoreValues(...ids) {
+	for (let id of ids) {
+		restoreValue(id);
+	}
+}
+
+// restoreValue restores the value of the element given by its ID from the local storage.
+function restoreValue(id) {
+	if (typeof(Storage) == "undefined") {
+		return; // No local storage support
+	}
+
+	var value = localStorage.getItem(id);
+	if (value === null) {
+		return;
+	}
+	var el = elByID(id);
+	if (el.tagName === "SELECT") {
+		// Only set value if it's a valid option:
+		for (o of el.options) {
+			if (o.value == value) {
+				el.value = value;
+				break
+			}
+		}
+	} else if (el.tagName === "INPUT" && el.type === "checkbox") {
+		el.checked = value == "true";
+	} else if (el.hasAttribute("value")) {
+		el.value = value;
+	} else {
+		el.textContent = value;
+	}
+}
+
+// setKeyValue stores the given value for the given key in the local storage.
+function setKeyValue(key, value) {
+	if (typeof(Storage) == "undefined") {
+		return; // No local storage support
+	}
+
+	localStorage.setItem(key, value);
+}
+
+// getKey returns the stored value for the given key from the local storage.
+function getKey(key) {
+	if (typeof(Storage) == "undefined") {
+		return; // No local storage support
+	}
+
+	return localStorage.getItem(key);
+}
+
+// delKeys deletes values of all listed keys from the local storage.
+function delKeys(...keys) {
+	for (let key of keys) {
+		delKey(key);
+	}
+}
+
+// delKey deletes the stored value for the given key from the local storage.
+function delKey(key) {
+	if (typeof(Storage) == "undefined") {
+		return; // No local storage support
+	}
+
+	localStorage.removeItem(key);
+}
